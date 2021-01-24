2851b4b7894ecabd062008b282105747bf6bd475
Alberto <alsemo.alberto@gmail.com>
2021-01-24 14:10:35 +0100

    Updates

---
+++ precache-manifest.17e8c37d38491fe599e6a6aa2de95093.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "056ce9508aef7030d528911f9866e6e6",
+    "url": "/SUPER-Board/index.html"
+  },
+  {
+    "revision": "f647589431d7e7282664",
+    "url": "/SUPER-Board/static/css/2.3fdf07a9.chunk.css"
+  },
+  {
+    "revision": "7b7438cde292e7b72a09",
+    "url": "/SUPER-Board/static/css/main.727b9347.chunk.css"
+  },
+  {
+    "revision": "f647589431d7e7282664",
+    "url": "/SUPER-Board/static/js/2.de9b7fb7.chunk.js"
+  },
+  {
+    "revision": "d3f045716c6141490dbf5c6d9d0c9b19",
+    "url": "/SUPER-Board/static/js/2.de9b7fb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b7438cde292e7b72a09",
+    "url": "/SUPER-Board/static/js/main.3c3e7083.chunk.js"
+  },
+  {
+    "revision": "9b54d546b817c2016da3",
+    "url": "/SUPER-Board/static/js/runtime-main.c283637e.js"
+  },
+  {
+    "revision": "c66d4f4b4adaf09f62eaa108be9d857d",
+    "url": "/SUPER-Board/static/media/DefaultUser.c66d4f4b.png"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/SUPER-Board/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/SUPER-Board/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/SUPER-Board/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/SUPER-Board/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/SUPER-Board/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/SUPER-Board/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/SUPER-Board/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/SUPER-Board/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/SUPER-Board/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/SUPER-Board/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/SUPER-Board/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/SUPER-Board/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/SUPER-Board/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/SUPER-Board/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/SUPER-Board/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/SUPER-Board/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
